bcf5552765537635b8c285a72a9baa3674716611
BadCubozoa <thespeedygenius@gmail.com>
2020-04-23 19:26:20 -0400

    put html back lol

--- assgn13/app.js
+++ assgn13/app.js
@@ -1,14 +1,10 @@
-var http = require('http');
+var http=require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write ("<head><title> Companies Form </title></head>");
-  res.write("<body><h1> Submit Company or Ticker </h1>");
-  res.write("<h3> In this form, choose to either submit a company name or a ticker symbol \
-  to see the related data. </h3> ");
-  res.write("<form name = 'myform'>");
-  res.write("<p>Company Name: <input type= 'text'  name='fname' placeholder='EX: Apple'/></p>");
-  res.write("<p>Ticker Symbol:<input type='text' name='lname' placeholder='EX: APPL'/></p>");
-  res.write("<input type = 'submit' value = 'Submit Choice'/>");
-  res.end('</form></body></html>');
-  }).listen(8080);
+var fs=require('fs');
+http.createServer(function(req,res) {
+  file="hello.html";
+  fs.readFile(file,function(err,txt) {
+    res.writeHead(200, {'Content‐Type':'text/html'});
+    res.write(txt);res.end();
+  });
+}).listen(8080);

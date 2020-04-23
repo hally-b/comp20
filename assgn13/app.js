67fd1a8f8ce1ba0c3a6e1cd53d18796c1574e552
BadCubozoa <thespeedygenius@gmail.com>
2020-04-23 19:12:22 -0400

    added html to node

--- assgn13/app.js
+++ assgn13/app.js
@@ -2,10 +2,13 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write ("<html><head><style type='text/css'>.col {display: inline-block; width:40px; \
-    border: 1px solid #333;} </style></head>");
-    res.write("<body>Calendar for April 2020<br />");
-    for (i=0; i<m.length; i++)
-      res.write(m[i] + "<br />")
-    res.end("</body></html>");
+  res.write ("<head><title> Companies Form </title></head>");
+  res.write("<body><h1> Submit Company or Ticker </h1>");
+  res.write("<h3> In this form, choose to either submit a company name or a ticker symbol \
+  to see the related data. </h3> ");
+  res.write("<form name = 'myform'>");
+  res.write("<p>Company Name: <input type= 'text'  name='fname' placeholder='EX: Apple'/></p>");
+  res.write("<p>Ticker Symbol:<input type='text' name='lname' placeholder='EX: APPL'/></p>");
+  res.write("<input type = 'submit' value = 'Submit Choice'/>");
+  res.end('</form></body></html>');
   }).listen(8080);
